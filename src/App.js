4249a761131aa6edfc6b98f5f2ae78eb48ce6aa8
Kevin Potschien <kpotschien@gmail.com>
2021-07-10 17:52:36 +0200

    save before switching to flex

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import './App.css';
+import './styles/App.css';
 import { getRandomData } from './data/sentences.js';
-import { Card } from './components/Card';
+import { Game } from './components/Game';
 import { useState, useEffect } from 'react';
 import { winHandler } from './data/winHandler.js';
 
@@ -8,17 +8,6 @@ function App() {
 	const [userArray, setUserArray] = useState(getRandomData());
 	const [wins, setWins] = useState();
 
-	const matchHandler = (e) => {
-		e.preventDefault();
-		setUserArray(
-			userArray.map((item, index) => {
-				if (index === +e.target.id) {
-					item.match = !item.match;
-				}
-				return item;
-			})
-		);
-	};
 	const resetHandler = () => {
 		let reseter = getRandomData();
 		setUserArray(reseter);
@@ -30,31 +19,15 @@ function App() {
 
 	return (
 		<div className='App'>
-			<div className='game'>
-				{userArray.map((item, index) => {
-					if (index === 12) {
-						return (
-							<Card
-								key={index}
-								sentence={'JOKER'}
-								match={(item.match = true)}
-								identifier={index}
-							/>
-						);
-					} else {
-						return (
-							<Card
-								matchHandler={matchHandler}
-								key={index}
-								sentence={item.sentence}
-								match={item.match}
-								identifier={index}
-							/>
-						);
-					}
-				})}
+			<div id='header'>
+				<h1>Bingo.</h1>
+			</div>
+			<main>
+				<Game userArray={userArray} setUserArray={setUserArray} />
+			</main>
+			<div id='footer'>
 				<button onClick={resetHandler}>Reset</button>
-				<div>{wins}</div>
+				<div>Wins : {wins}</div>
 			</div>
 		</div>
 	);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,6 +5,13 @@ export const Card = ({ sentence, matchHandler, match, identifier }) => {
 	let className = 'card';
 	className += match ? ' match' : ' noMatch';
 
+	if (identifier === 12) {
+		return (
+			<div id={identifier} className='card'>
+				{'JOKER'}
+			</div>
+		);
+	}
 	return (
 		<div onClick={matchHandler} id={identifier} className={className}>
 			{sentence}
