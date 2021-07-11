2577092bcc2ad7663fafb26ebc2cf809194769f7
Kevin Potschien <kpotschien@gmail.com>
2021-07-11 14:09:48 +0200

    added easter-egg

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../styles/Card.css';
 import cross from '../imgs/cross.svg';
+import cat from '../imgs/cat.svg';
 
 export const Card = ({ sentence, matchHandler, match, identifier }) => {
 	let className = 'card';
@@ -11,7 +12,6 @@ export const Card = ({ sentence, matchHandler, match, identifier }) => {
 			<div className='square'>
 				<div id={identifier} className='card joker match'>
 					<img className='cross' src={cross} alt='cross' />
-					<div className='card--text__joker'>Joker</div>
 				</div>
 			</div>
 		);
@@ -19,7 +19,13 @@ export const Card = ({ sentence, matchHandler, match, identifier }) => {
 	return (
 		<div className='square'>
 			<div onClick={matchHandler} id={identifier} className={className}>
-				{match && <img className='cross' src={cross} alt='cross' />}
+				{match && (
+					<img
+						className='cross'
+						src={sentence === "~there's a cat~" ? cat : cross}
+						alt='cross'
+					/>
+				)}
 				<div className='card--text'>{sentence}</div>
 			</div>
 		</div>
